0f520f318458d1604a79864bac32582e79784545
Sachin Choudhary <sachin.choudhary@punchh.com>
2021-04-18 12:59:55 +0530

    initial commit with basic code structure

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+var showSecret = false;
+
+secretButton.addEventListener('click', toggleSecretState);
+updateSecretParagraph();
+
+function toggleSecretState() {
+    showSecret = !showSecret;
+    updateSecretParagraph();
+    updateSecretButton()
+}
+
+function updateSecretButton() {
+    if (showSecret) {
+        secretButton.textContent = 'Hide the Secret';
+    } else {
+        secretButton.textContent = 'Show the Secret';
+    }
+}
+
+function updateSecretParagraph() {
+    if (showSecret) {
+        secretParagraph.style.display = 'block';
+    } else {
+        secretParagraph.style.display = 'none';
+    }
+}
